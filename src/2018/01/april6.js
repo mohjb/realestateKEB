3ec141cde4d8121f75e587a8adb0831263c238d6
Mohamad Bo-Hamad <mohjb@users.noreply.github.com>
2018-04-10 11:17:57 +0300

    Update april6.js

--- src/2018/01/april6.js
+++ src/2018/01/april6.js
@@ -46,22 +46,71 @@ xUrl='/txtSrvlt/';//2017.11.jsp
 			if( p.lsSaved<x.lastModified)
 				x.serverStore()
 	}}//else console.log('main.blur:no unique id found:',x)
-	,txtNode:{path:[{id:1,key:'key2'},{id:1,key:'key1'}]//path
+	/*,txtNode:{path:[{id:1,key:'key2'},{id:1,key:'key1'}]//path
 		,txt:{id:3,key:'key3',owner:'moh',group:'moh',parent:2,perm:47
 			,txt:'hi',meta:{protoId:3,typ:'node',yes:true}}//txt
 		,children:[{id:4,key:'key4'},{id:5,key:'key5'}]//children
-		}//txtNode
+		}//txtNode*/
+	,txt:{0:{id:0,parent:0,key:'root',owner:'moh',group:'moh',perm:0
+			,txt:'',meta:{path:[0],children:[1,2]}}
+		,1:{id:1,parent:0,key:'users',owner:'moh',group:'moh',perm:47
+			,txt:'',meta:{path:[0],children:[3]}}
+		,2:{id:2,parent:0,key:'apps',owner:'moh',group:'moh',perm:47
+			,txt:'',meta:{path:[0],children:[4]}}
+		,3:{id:3,parent:1,key:'moh',owner:'moh',group:'moh',perm:47
+			,txt:'',meta:{path:[1,0],children:[]}}
+		,4:{id:4,parent:2,key:'app1',owner:'moh',group:'moh',perm:47
+			,txt:'',meta:{path:[2,0],children:[5,6]}}
+		,5:{id:5,parent:4,key:'key1',owner:'moh',group:'moh',perm:47
+			,txt:'',meta:{path:[4,2,0],children:[]}}
+		,6:{id:6,parent:4,key:'key2',owner:'moh',group:'moh',perm:47
+			,txt:'',meta:{path:[4,2,0],children:[]}}
+	}//Txt
+	,selected:{id:4}
+	,load:function load(id,onload,ref){
+		var x=main.txt[id];if(onload){
+			if(onload instanceof Function)
+				onload(x,ref);
+			else 
+				onload.onload(x,ref)}
+		return x;}
+	/*//,children:{0:[1,2],1:[3],2:[4],3:[],4:[5,6],5:[],6:[]}
+	,loadNode:function loadNode(id,onload,ref){
+		var x=main.load(id)
+		return x;}*/
 	}//main
-
 	return p;//function mainFactoryCallback(message) {return p;}
 }])
 .controller('mainCtrl',function mainCtrlController($scope,main ) {
-	if(!main || !main.usr)
+	if(!main )//|| !main.usr
 		return location.hash='#!/login'
 	if(main){
 		$scope.main=main
 		$scope.dt=dt
-		$scope.txtNode=main.txtNode
+		$scope.selected=main.selected
+		$scope.txt=main.load(main.selected.id)
+		$scope.clk=function(id,prnt,ky){
+			main.load(id,function(x,ref){main.selected.id=id;$scope.txt=x;});}
+		$scope.onNewChild=function onNewChild(prnt){
+			var y='',p=$scope.txt,path=[p.id]
+			,x={id:id,parent:id,key:ky,owner:'moh',group:'moh',perm:47
+				,txt:'',meta:{path:path,children:[]}};
+			while(main.txt[x.id])
+				x.id++
+			function c(k,x){
+				var a=x.meta.children;if(a)for(var i in a){
+					var b=main.txt[a[i]];if(b.key==k)return b;}
+				return null;}
+			while((y=prompt('Please enter a unique key')) && c(y,p));
+			if(y){x.key=y;
+				p.meta.children.push(x.id)
+				var a=p.meta.path
+				for(var i in a)path.push(a[i]);
+				main.txt[x.id]=x;
+				main.selected.id=x.id;$scope.txt=x;
+				return x
+			}return null
+		}//onNewChild
 	}else{
 		$scope.usr=null;
 		console.log('mainCntrl:version=',$scope.version='mainCntrl , no app')
@@ -87,51 +136,3 @@ xUrl='/txtSrvlt/';//2017.11.jsp
 
 
 })//config
-.directive('txtnode',function($compile){
-	var linker=function(scope,element,attrs){
-		var x=scope.txtNode=main.txtNode
-		,b=['<h1>{{txtNode.txt.id}}|{{txtNode.txt.key}}</h1><h3>Path</h3>'],c,a=x.path;
-		function btn(p){b.push('<button>',p.id,'|',p.key,'</button>');}
-		for(var i in a){b.push(' / ');btn(a[i])}
-		b.push('<h3>Node</h3>'
-			,'Id:<input ng-moedl="txtNode.txt.id"/>'
-			,' , Key:<input ng-model="txtNode.txt.key"/>'
-			,' , owner:<input ng-model="txtNode.txt.owner"/>'
-			,' , group:<input ng-model="txtNode.txt.group"/>'
-			,' , perm:<input ng-model="txtNode.txt.perm"/>'
-			,' , meta:<input ng-model="txtNode.txt.meta"/>'
-			,' , txt:<input ng-model="txtNode.txt.txt"/>');
-		b.push('<h3>Children</h3>');
-		a=x.children
-		for(var i in a){b.push('<br/>');btn(a[i]);}
-		element.innerHTML=b.join('')
-/*
-
-.directive('tmplt',function($compile){
-    var t1='<div>{{rootDirectory}}{{content.data }}</div>'
-    var linker=function(scope,element,attrs){
-        scope.rootDirectory='images/';
-        element.innerHTML=t1//.html(t1)//.show();
-        $compile(element.contents())(scope);
-    }//linker
-    return {restrict:'E',link:linker, scope:{content:'='}};
-})
-.directive('txtNodeTxt',function($compile){
-	var t1='Id:<input ng-moedl="txtNode.txt.id"/> , Key:<input ng-model="txtNode.txt.key"/> , owner:<input ng-model="txtNode.txt.owner"/> , group:<input ng-model="txtNode.txt.group"/> , perm:<input ng-model="txtNode.txt.perm"/> , meta:<input ng-model="txtNode.txt.meta"/> , txt:<input ng-model="txtNode.txt.txt"/>'
-	var linker=function(scope,element,attrs){
-		element.innerHTML=t1
-		$compile(element.contents())(scope);
-	}//linker
-	return {restrict:'E',link:linker, scope:{content:'='}};})
-.directive('txtNodeChildren',function($compile){
-	var linker=function(scope,element,attrs){
-		var a=txtNode.children,b=[],c;
-		for(var i in a){c=a[i];b.push('<br/><button>'+c.key+'@'+c.id+'</button>');}
-		element.innerHTML=b.join('')
-		$compile(element.contents())(scope);
-	}//linker
-	return {restrict:'E',link:linker, scope:{content:'='}};})
-*/
-		$compile(element.contents())(scope);
-	}//linker
-	return {restrict:'E',link:linker, scope:{content:'='}};})
